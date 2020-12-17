0868eedcd5aaebdf1170e96b3895ffdbd895dca0
melonsound <ghost.134@hotmail.com>
2020-12-17 11:18:03 +0300

    Добавление шаблонов в проект

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require 'view/template/header.php'; ?>
+
+<!-- Content for home page -->
+
+<?php require 'view/template/footer.php'; ?>
+
+
---
+++ view/template/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Главная</title>
+</head>
+
+<body>
+    <!--Header-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Меню</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Главная <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Заказать</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Список заявок</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!--/Header-->
